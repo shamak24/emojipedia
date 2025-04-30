216a75ed230360e9ffda840381cea6bacdb77c2c
Shamak <rishitroyr2535@gmail.com>
2025-04-30 23:26:04 +0530

    first

---
+++ src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "This slightly smiling emoji expresses a pleasant contentment and inner peace. It is used to make a statement seem friendlier, even if it is not necessarily meant that way.",
+  },
+  {
+    id: 2,
+    emoji: "😌",
+    name: "Relieved Face",
+    meaning:
+      "Everything went well! The relieved face 😌 radiates a calming atmosphere. It is a gentle smile that signals that an unpleasant situation has been successfully handled. Worries and fears can now be put aside as the person feels relaxed and resolved.",
+  },
+  {
+    id: 3,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A winking 😉 emoji indicates someone is being mischievous or humorous. It shows that someone is joking, flirting, or has a hidden agenda. This emoji is a friendly way to defuse a message and indicate that it should be taken with a grain of salt.",
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Nerd emoji 🤓 is a face with big glasses, an awkward smile and buck teeth. It's often used in a humorous or ironic way to describe nerds or to express how smart you are. It represents the stereotype of the nerd, an intelligent but oddly dressed person with social deficits.",
+  },
+];
+
+export default emojipedia;
